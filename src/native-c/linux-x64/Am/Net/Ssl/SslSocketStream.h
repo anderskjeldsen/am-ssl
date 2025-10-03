7057e81e282ec9c5ae293d89a6b95819a6b11da3
Anders Kjeldsen <anders@appify.no>
2025-10-03 17:44:03 +0200

    fixed ref to socket

--- src/native-c/linux-x64/Am/Net/Ssl/SslSocketStream.h
+++ src/native-c/linux-x64/Am/Net/Ssl/SslSocketStream.h
@@ -3,7 +3,7 @@
 #include <Am/Net/Ssl/SslSocketStream.h>
 #include <Am/IO/Stream.h>
 #include <Am/Lang/Object.h>
-#include <Am/IO/Networking/Socket.h>
+#include <Am/Net/Socket.h>
 #include <Am/Lang/String.h>
 #include <Am/Lang/Int.h>
 #include <Am/Lang/Array.h>
